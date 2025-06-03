11cff57020c48e47f394a62a2aadcf25932910cf
Peter Smek <smek.peter@gmail.com>
2025-06-03 21:40:23 +0200

    Added halt alias for break command

--- axiom-cli/src/main/kotlin/net/voxelpi/axiom/cli/emulator/command/EmulatorBreakCommand.kt
+++ axiom-cli/src/main/kotlin/net/voxelpi/axiom/cli/emulator/command/EmulatorBreakCommand.kt
@@ -1,22 +1,31 @@
 package net.voxelpi.axiom.cli.emulator.command
 
+import com.github.ajalt.mordant.rendering.TextColors
 import net.voxelpi.axiom.cli.command.AxiomCommandManager
 import net.voxelpi.axiom.cli.command.AxiomCommandProvider
+import net.voxelpi.axiom.cli.command.AxiomCommandSender
 import net.voxelpi.axiom.cli.emulator.Emulator.Companion.PREFIX_EMULATOR
 import net.voxelpi.axiom.cli.emulator.computer.EmulatedComputer
+import org.incendo.cloud.context.CommandContext
 import org.incendo.cloud.kotlin.extension.buildAndRegister
 
 class EmulatorBreakCommand(val computer: EmulatedComputer) : AxiomCommandProvider {
 
     override fun registerCommands(commandManager: AxiomCommandManager) {
         commandManager.buildAndRegister("break") {
-            handler { context ->
-                if (!computer.isExecuting()) {
-                    context.sender().terminal.writer().println("$PREFIX_EMULATOR Computer is currently not running.")
-                }
-                computer.halt()
-                context.sender().terminal.writer().println("$PREFIX_EMULATOR Stopped the computer.")
-            }
+            handler(::haltComputer)
         }
+        commandManager.buildAndRegister("halt") {
+            handler(::haltComputer)
+        }
+    }
+
+    private fun haltComputer(context: CommandContext<AxiomCommandSender>) {
+        if (!computer.isExecuting()) {
+            context.sender().terminal.writer().println("$PREFIX_EMULATOR Computer is currently ${TextColors.brightRed("not running")}")
+            return
+        }
+        computer.halt()
+        context.sender().terminal.writer().println("$PREFIX_EMULATOR Stopped the computer")
     }
 }
