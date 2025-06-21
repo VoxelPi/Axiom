96e7a49c8e9beac32ffe18d77cddd173e13166cc
Peter Smek <smek.peter@gmail.com>
2025-06-21 12:22:38 +0200

    Add number of instructions flag to run command

--- axiom-cli/src/main/kotlin/net/voxelpi/axiom/cli/emulator/command/EmulatorRunCommand.kt
+++ axiom-cli/src/main/kotlin/net/voxelpi/axiom/cli/emulator/command/EmulatorRunCommand.kt
@@ -5,8 +5,10 @@ import net.voxelpi.axiom.cli.command.AxiomCommandManager
 import net.voxelpi.axiom.cli.command.AxiomCommandProvider
 import net.voxelpi.axiom.cli.emulator.Emulator.Companion.PREFIX_EMULATOR
 import net.voxelpi.axiom.cli.emulator.computer.EmulatedComputer
+import org.incendo.cloud.kotlin.extension.argumentDescription
 import org.incendo.cloud.kotlin.extension.buildAndRegister
 import org.incendo.cloud.parser.standard.IntegerParser.integerParser
+import kotlin.jvm.optionals.getOrNull
 
 class EmulatorRunCommand(val computer: EmulatedComputer) : AxiomCommandProvider {
 
@@ -14,40 +16,19 @@ class EmulatorRunCommand(val computer: EmulatedComputer) : AxiomCommandProvider 
         commandManager.buildAndRegister("run") {
             flag("trace", aliases = arrayOf("t"))
             flag("silent", aliases = arrayOf("s"))
+            flag("number", aliases = arrayOf("n"), argumentDescription("Number of instructions to execute."), integerParser(0))
 
             handler { context ->
                 val trace = context.flags().isPresent("trace")
                 val silent = context.flags().isPresent("silent")
+                val nScheduledInstructions = context.flags().getValue<Int>("number").getOrNull()
 
                 if (computer.isExecuting()) {
                     context.sender().terminal.writer().println("$PREFIX_EMULATOR Computer is already running.")
                     return@handler
                 }
-                computer.run(trace = trace, silent = silent) { nInstructions ->
-                    context.sender().lineReader.printAbove("$PREFIX_EMULATOR Computer ${TextColors.brightGreen("finished")} executing ${TextColors.brightYellow(nInstructions.toString())} instructions")
-                }.getOrElse {
-                    context.sender().terminal.writer().println("$PREFIX_EMULATOR Failed to run: ${it.message}")
-                }
-            }
-        }
-
-        commandManager.buildAndRegister("run") {
-            literal("next")
-            optional("n_instructions", integerParser(0))
-            flag("trace", aliases = arrayOf("t"))
-            flag("silent", aliases = arrayOf("s"))
-
-            handler { context ->
-                val nScheduledInstructions: Int = context.getOrDefault("n_instructions", Integer.MAX_VALUE)
-                val trace = context.flags().isPresent("trace")
-                val silent = context.flags().isPresent("silent")
-
-                if (computer.isExecuting()) {
-                    context.sender().terminal.writer().println("$PREFIX_EMULATOR Computer is already running.")
-                    return@handler
-                }
-                computer.run(nScheduledInstructions, trace = trace, silent = silent) { nInstructions ->
-                    if (nInstructions < nScheduledInstructions) {
+                computer.run(nScheduledInstructions ?: Int.MAX_VALUE, trace = trace, silent = silent) { nInstructions ->
+                    if (nScheduledInstructions != null && nInstructions < nScheduledInstructions) {
                         context.sender().lineReader.printAbove("$PREFIX_EMULATOR Computer entered ${TextColors.brightRed("break")} after executing ${TextColors.brightYellow(nInstructions.toString())} / ${TextColors.brightYellow(nScheduledInstructions.toString())} instructions")
                     } else {
                         context.sender().lineReader.printAbove("$PREFIX_EMULATOR Computer ${TextColors.brightGreen("finished")} executing ${TextColors.brightYellow(nInstructions.toString())} instructions")
